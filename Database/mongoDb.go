face348d7f37f675b064c40692b3feb7c4da8e2d
Sadık Sünbül <109942759+SadikSunbul@users.noreply.github.com>
2025-01-11 16:22:38 +0300

    up

--- Database/mongoDb.go
+++ Database/mongoDb.go
@@ -8,34 +8,8 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-// ICollection, koleksiyon işlemleri için interface
-type ICollection interface {
-	InsertOne(ctx context.Context, document interface{}) (*mongo.InsertOneResult, error)
-	UpdateOne(ctx context.Context, filter interface{}, update interface{}) (*mongo.UpdateResult, error)
-	DeleteOne(ctx context.Context, filter interface{}) (*mongo.DeleteResult, error)
-}
-
-// IDatabase, veritabanı işlemleri için interface
-type IDatabase interface {
-	Collection(name string, opts ...*options.CollectionOptions) *mongo.Collection
-	RunCommand(ctx context.Context, runCommand interface{}) *mongo.SingleResult
-}
-
 type DataBase struct {
-	Client IDatabase
-}
-
-// DatabaseWrapper, gerçek MongoDB veritabanını sarmalar
-type DatabaseWrapper struct {
-	*mongo.Database
-}
-
-func (d *DatabaseWrapper) Collection(name string, opts ...*options.CollectionOptions) *mongo.Collection {
-	return d.Database.Collection(name, opts...)
-}
-
-func (d *DatabaseWrapper) RunCommand(ctx context.Context, runCommand interface{}) *mongo.SingleResult {
-	return d.Database.RunCommand(ctx, runCommand)
+	Client *mongo.Database
 }
 
 func ConnectionDatabase() *DataBase {
@@ -45,5 +19,5 @@ func ConnectionDatabase() *DataBase {
 	if err != nil {
 		panic(err)
 	}
-	return &DataBase{Client: &DatabaseWrapper{client.Database(config.DbName)}}
+	return &DataBase{Client: client.Database(config.DbName)}
 }
